7b931faa9796a7e6057b6fa58afcdd55970862b4
huihui.li <lihuihui@wizarpos.com>
2025-05-23 16:26:26 +0800

    replace AIDL to SDK

--- app/src/main/java/com/cloudpos/scannerinsetmodedemo/ScanActivity.java
+++ app/src/main/java/com/cloudpos/scannerinsetmodedemo/ScanActivity.java
@@ -3,30 +3,33 @@ package com.cloudpos.scannerinsetmodedemo;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.os.RemoteException;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.cloudpos.scanserver.aidl.IScanService;
-import com.cloudpos.scanserver.aidl.ScanParameter;
-import com.cloudpos.scanserver.aidl.ScanResult;
-import com.cloudpos.scannerinsetmodedemo.aidlcontrol.AidlController;
-import com.cloudpos.scannerinsetmodedemo.aidlcontrol.IAIDLListener;
+import com.cloudpos.DeviceException;
+import com.cloudpos.advance.ext.POSTerminalAdvance;
+import com.cloudpos.advance.ext.scanner.IScannerDevice;
+import com.cloudpos.advance.ext.scanner.ScanParameter;
+import com.cloudpos.advance.ext.scanner.ScanResult;
 import com.cloudpos.scannerinsetmodedemo.utils.DeviceUtils;
 
 /**
  * Created by gecx on 17-1-20.
  */
 
-public class ScanActivity extends Activity implements IAIDLListener, View.OnClickListener {
+public class ScanActivity extends Activity implements View.OnClickListener {
 
     private Context context;
     private String TAG = "ScanActivity";
+    private IScannerDevice scannerDevice;
+
+    boolean flashlight;
+    boolean indicatorlight;
+    int cameraIndex;
 
 
     @Override
@@ -49,145 +52,129 @@ public class ScanActivity extends Activity implements IAIDLListener, View.OnClic
         changeflashlight.setOnClickListener(this);
 
         title.setText("Scan");
+        scannerDevice = POSTerminalAdvance.getInstance().getScannerDevice();
+        open();
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+               close();
+            }
+        });
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.changecamera:
+                sendBroadcastForCameraSwitch();
+                break;
+            case R.id.changeflashlight:
+                flashlight = !flashlight;
+                sendScanBroadcast(ScanParameter.BROADCAST_SET_FLASHLIGHT, flashlight);
+                break;
+            case R.id.changeindicator:
+                indicatorlight = !indicatorlight;
+                sendScanBroadcast(ScanParameter.BROADCAST_SET_INDICATOR, indicatorlight);
+
+                break;
+        }
+    }
+
+    private void sendBroadcastForCameraSwitch() {
+        Intent intent = new Intent();
+        intent.setAction(ScanParameter.BROADCAST_SET_CAMERA);
+        cameraIndex = cameraIndex == 0 ? 1 : 0;
+        intent.putExtra(ScanParameter.BROADCAST_VALUE, cameraIndex);
+        sendBroadcast(intent);
+    }
+
+    private void sendScanBroadcast(String action, boolean value) {
+        Intent intent = new Intent();
+        intent.setAction(action);
+        intent.putExtra(ScanParameter.BROADCAST_VALUE, value);
+        sendBroadcast(intent);
+    }
+
+    private void open(){
+        runInBackground(new Runnable() {
+            @Override
+            public void run() {
                 try {
-                    scanService.stopScan();
-                } catch (RemoteException e) {
+                    boolean result = scannerDevice.open(context);
+                    if(result){
+                        beginScan();
+                    }
+                } catch (DeviceException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+    private void close(){
+        runInBackground(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    scannerDevice.stopScan();
+                    scannerDevice.close();
+                } catch (DeviceException e) {
                     e.printStackTrace();
                 }
             }
         });
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        AidlController.getInstance().startScanService(context, this);
-
-    }
-
-    boolean flashlight;
-    boolean indicatorlight;
-    int cameraIndex;
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.changecamera:
-                Intent intent1 = new Intent();
-                intent1.setAction(ScanParameter.BROADCAST_SET_CAMERA);
-                if (cameraIndex == 0) {
-                    intent1.putExtra(ScanParameter.BROADCAST_VALUE, 1);
-                    cameraIndex = 1;
-                } else if (cameraIndex == 1) {
-                    intent1.putExtra(ScanParameter.BROADCAST_VALUE, 0);
-                    cameraIndex = 0;
-                }
-                sendBroadcast(intent1);
-
-                break;
-            case R.id.changeflashlight:
-                Intent intent2 = new Intent();
-                intent2.setAction(ScanParameter.BROADCAST_SET_FLASHLIGHT);
-                if (flashlight) {
-                    intent2.putExtra(ScanParameter.BROADCAST_VALUE, false);
-                    flashlight = false;
-                } else {
-                    intent2.putExtra(ScanParameter.BROADCAST_VALUE, true);
-                    flashlight = true;
-                }
-                sendBroadcast(intent2);
-                break;
-            case R.id.changeindicator:
-                Intent intent3 = new Intent();
-                intent3.setAction(ScanParameter.BROADCAST_SET_INDICATOR);
-                if (indicatorlight) {
-                    intent3.putExtra(ScanParameter.BROADCAST_VALUE, false);
-                    indicatorlight = false;
-                } else {
-                    intent3.putExtra(ScanParameter.BROADCAST_VALUE, true);
-                    indicatorlight = true;
-                }
-                sendBroadcast(intent3);
-
-                break;
-        }
-    }
-
     // It's necessary to calculate the heights of both the status bar and the title bar, with the final values to be provided in density-independent pixels (dp).
     public void beginScan() {
-        int screenHeight = DeviceUtils.px2dp(context, DeviceUtils.getScreenHeight(context));
-        int windowWidth = DeviceUtils.px2dp(context, DeviceUtils.getScreenWidth(context));
-        int statusHeight = DeviceUtils.px2dp(context, DeviceUtils.getStausHeight(context));
-        int windowHeight = screenHeight - 60 - statusHeight - 60; // 60 is the heights of both the status bar and the title bar
-        ScanParameter parameter = new ScanParameter();
-        parameter.set(ScanParameter.KEY_UI_WINDOW_TOP, 60 + 60);
-        parameter.set(ScanParameter.KEY_UI_WINDOW_LEFT, 0);
-        parameter.set(ScanParameter.KEY_UI_WINDOW_WIDTH, windowWidth);
-        parameter.set(ScanParameter.KEY_UI_WINDOW_HEIGHT, windowHeight);
-        parameter.set(ScanParameter.KEY_ENABLE_FLASH_ICON, false);
-        parameter.set(ScanParameter.KEY_ENABLE_SWITCH_ICON, false);
-        parameter.set(ScanParameter.KEY_ENABLE_INDICATOR_LIGHT, false);
-        parameter.set(ScanParameter.KEY_SCAN_TIP_TEXT, "");
-        parameter.set(ScanParameter.KEY_SCAN_SECTION_WIDTH, windowWidth);
-        parameter.set(ScanParameter.KEY_SCAN_SECTION_HEIGHT, windowHeight);
-        parameter.set(ScanParameter.KEY_SCAN_MODE, "overlay");
+        runInBackground(() -> {
+            int screenHeight = DeviceUtils.px2dp(context, DeviceUtils.getScreenHeight(context));
+            int windowWidth = DeviceUtils.px2dp(context, DeviceUtils.getScreenWidth(context));
+            int statusHeight = DeviceUtils.px2dp(context, DeviceUtils.getStausHeight(context));
+            int windowHeight = screenHeight - 60 - statusHeight - 60; // 60 is the heights of both the status bar and the title bar
+            ScanParameter parameter = new ScanParameter();
+            parameter.set(ScanParameter.KEY_UI_WINDOW_TOP, 60 + 60);
+            parameter.set(ScanParameter.KEY_UI_WINDOW_LEFT, 0);
+            parameter.set(ScanParameter.KEY_UI_WINDOW_WIDTH, windowWidth);
+            parameter.set(ScanParameter.KEY_UI_WINDOW_HEIGHT, windowHeight);
+            parameter.set(ScanParameter.KEY_ENABLE_FLASH_ICON, false);
+            parameter.set(ScanParameter.KEY_ENABLE_SWITCH_ICON, false);
+            parameter.set(ScanParameter.KEY_ENABLE_INDICATOR_LIGHT, false);
+            parameter.set(ScanParameter.KEY_SCAN_TIP_TEXT, "");
+            parameter.set(ScanParameter.KEY_SCAN_SECTION_WIDTH, windowWidth);
+            parameter.set(ScanParameter.KEY_SCAN_SECTION_HEIGHT, windowHeight);
+            parameter.set(ScanParameter.KEY_SCAN_MODE, "overlay");
 
-        try {
-            ScanResult scannerResult = scanService.scanBarcode(parameter);
-            Log.d(TAG, "stopScan:" + scannerResult);
-            String msg;
-            if (scannerResult.getResultCode() == ScanResult.SCAN_SUCCESS) {
-                msg = scannerResult.getText() + "\ncode = " + scannerResult.getResultCode();
-            } else {
-                msg = "errorcode = " + scannerResult.getResultCode();
+            try {
+                ScanResult scannerResult = scannerDevice.scanBarcode(parameter);
+                Log.d(TAG, "stopScan:" + scannerResult);
+                String msg;
+                if (scannerResult.getResultCode() == ScanResult.SCAN_SUCCESS) {
+                    msg = scannerResult.getText() + "\ncode = " + scannerResult.getResultCode();
+                } else {
+                    msg = "errorcode = " + scannerResult.getResultCode();
+                }
+                runOnUiThread(() -> {
+                    Intent intent = new Intent();
+                    intent.putExtra("result", msg);
+                    setResult(2, intent);
+                    finish();
+                });
+            } catch (DeviceException e) {
+                e.printStackTrace();
             }
-            Intent intent = new Intent();
-            intent.putExtra("result", msg);
-            setResult(2, intent);
-            finish();
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
+        });
     }
+
+    private void runInBackground(Runnable task) {
+        new Thread(task).start();
+    }
+
 
     @Override
     protected void onPause() {
-        try {
-            scanService.stopScan();
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
         super.onPause();
+        close();
     }
 
-    @Override
-    protected void onDestroy() {
-        if(scanService != null){
-            this.unbindService(scanConn);
-            scanService = null;
-            scanConn = null;
-        }
-        super.onDestroy();
-    }
-    private IScanService scanService; //Scan code service
-
-    private ServiceConnection scanConn;
-
-
-    @Override
-    public void serviceConnected(Object objService, ServiceConnection connection) {
-        if(objService instanceof IScanService){
-            scanService = (IScanService) objService;
-            scanConn = connection;
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    beginScan();
-                }
-            }).start();
-        }
-    }
 }
